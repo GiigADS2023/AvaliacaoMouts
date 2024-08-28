7a072b70aaa539415496d0c477a2b89df919e6f9
Giorgia <giorgiabschmidtADS@gmail.com>
2024-08-28 15:55:59 -0300

    Classe venda realizada, ReposiorioVenda realizada.

---
+++ AvaliacaoMouts/RepositorioVenda.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaliacaoMouts
+{
+    internal class RepositorioVenda : IRepositorio<Venda, long>
+    {
+        public bool Alterar(long id, Venda item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Venda> Consultar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Venda Consultar(long item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Venda item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Venda Salvar(Venda item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
