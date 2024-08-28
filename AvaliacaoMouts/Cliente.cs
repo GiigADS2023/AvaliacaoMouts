070f18b6ee4834769a7b4b8ea1acfe98423d826f
Giorgia <giorgiabschmidtADS@gmail.com>
2024-08-28 14:46:32 -0300

    Adicionar arquivos de projeto.

---
+++ AvaliacaoMouts/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaliacaoMouts
+{
+    internal class Cliente : Pessoa
+    {
+        public string CPFCNPJ { get; set; }
+        public int PontoFidelidade { get; set; }
+
+        public Cliente(long id, string nome, string endereco, string cpfcnpj) : base(id, nome, endereco, cpfcnpj)
+        {
+            PontoFidelidade = 0;
+        }
+        public override bool validarIdentidade()
+        {
+            if (CPFCNPJ.Length == 14)
+            {
+                return CPFCNPJ.Length == 14;
+            } else if (CPFCNPJ.Length == 11)
+            {
+                return CPFCNPJ.Length == 11;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AvaliacaoMouts/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaliacaoMouts
+{
+    abstract class Pessoa
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string CpfCnpj { get; set; }
+
+        public abstract bool validarIdentidade();
+
+        public Pessoa(long id, string nome, string endereco, string cpfcnpj)
+        {
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+            CpfCnpj = cpfcnpj;
+        }
+    }
+}
